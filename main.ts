900a384e740ac61904bc24c511c8ad4457f1e61b
Snoopy1994 <87963937+Snoopy1994@users.noreply.github.com>
2022-03-01 11:08:55 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,13 +6,16 @@ input.onButtonPressed(Button.AB, function () {
     radio.sendValue("id", 17)
     radio.sendValue("try", target)
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 input.onButtonPressed(Button.B, function () {
     target += 1
+})
+radio.onReceivedValue(function (name, value) {
+	
 })
 let target = 0
 radio.setGroup(99)
 basic.showIcon(IconNames.Heart)
 target = 50
-basic.forever(function () {
-	
-})
